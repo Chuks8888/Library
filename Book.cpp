ed9c30d0ae371fa5d9e2a0e107c8d62ae9340f11
Chuks888 <hugotarczynski@gmail.com>
2024-03-10 22:31:52 +0100

    new way do to list

--- Book.cpp
+++ Book.cpp
@@ -1,32 +1,43 @@
 #include <iostream>
+#include <string.h>
 
 class Book
 {
     private:
         const char* Name;
-        const char* Author;
+        const char* Author; 
         int Pages;
+        int Availible;
 
     public:
-        Book(const char* name, char* author = "Noname", int pages);
+        Book(const char* name = " ", const char* author = " ", int pages = 0, int Availible = 0);
         void print();
-        
-
 };
-Book::Book(const char* name, char* author = "Noname", int pages)
+
+class List_of_books
+{
+    private:
+        Book *ptr;
+        List_of_books *next;
+    public:
+        List_of_books()  
+};
+
+Book::Book(const char* name, const char* author, int pages, int availible)
 {
     Name = name;
     Author = author;
     Pages = pages;
+    Availible = availible;
 }
 void Book::print()
 {
     std::cout << "Book: " << Name << std::endl;
     std::cout << "Author: " << Author << std::endl;
     std::cout << "Pages: " << Pages << std::endl;
+    std::cout << "Copies availible: " << Pages << std::endl;
 }
 
 int main()
 {
-    return 0;
 }
